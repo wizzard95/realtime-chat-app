14254b49d1f942988d6041f5996b57621219001a
wizzard95 <damian110995a@gmail.com>
2025-08-21 14:03:59 -0400

    se incorporo un .env en client, en el socket.js se definio la ruta hacia env y se reviso el package json

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,7 @@
-import { io } from 'socket.io-client';
+import { io } from "socket.io-client";
 
-const socket = io('http://localhost:3000'); // ajusta si usas .env
+const socket = io(import.meta.env.VITE_API_URL, {
+  withCredentials: true
+});
 
 export default socket;
