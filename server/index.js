89afe4257cb561c93142dcd76cb8607d0683e6f1
wizzard95 <damian110995a@gmail.com>
2025-08-21 14:19:45 -0400

    cambiando modificando el archivo index del server

--- server/index.js
+++ server/index.js
@@ -26,6 +26,12 @@ const io = new Server(server, {
 app.use(cors());
 app.use(express.json());
 
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
+}));
+
+
 // MongoDB conexión
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
